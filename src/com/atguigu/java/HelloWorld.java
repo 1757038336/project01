ea640103af83dd2d4b77f3085f72e2ade7e79c55
1757038336 <1757038336@qq.com>
2018-08-01 00:18:25 +0800

    测试提交到github

--- src/com/atguigu/java/HelloWorld.java
+++ src/com/atguigu/java/HelloWorld.java
@@ -4,6 +4,9 @@ package com.atguigu.java;
 public class HelloWorld {
     public static void main(String[] args){
         System.out.println("hello");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
     }
 
 }
